aa5da725e35619df274faa1efc16004ad1001480
zoperpogi <zoperpogi@gmail.com>
2021-03-18 08:28:08 +0800

    Initial Commit

---
+++ ProgramTrial.java
@@ -0,0 +1,12 @@
+public class ProgramTrial{
+    public static void main(String[]args){
+    
+    }
+}
+class Calculator{
+
+  public void add(double a, double b){
+     double add = a+b;
+     return add;
+  }
+}
